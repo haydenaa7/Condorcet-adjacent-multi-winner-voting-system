78d91e502a86a31220a8b086301efe12c268f3fa
haydenaa7 <harnold@arizona.edu>
2024-02-12 12:03:28 -0700

    added csv support to generator

--- generator.py
+++ generator.py
@@ -1,25 +1,23 @@
 import argparse
 import json
-import time
 import math
 import random
 
 '''
-Author: Hayden Arnold
-Course: CSC 496
-Assignment: A1 - Random Election Generator
+Random Election Generator
 '''
 
 def arguments():
     '''Get command line arguments'''
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num-voters", dest="voters", default=10)
-    parser.add_argument("--num-candidates", dest="candidates", default=3)
-    parser.add_argument("--max-unique-rankings", dest="unique", default=10e10)
-    parser.add_argument("--max-ranking-length", dest="max")
-    parser.add_argument("--min-ranking-length", dest="min")
-    parser.add_argument("--num-elections", dest="elections", default=1)
-    parser.add_argument("--output-file", dest="output")
+    parser.add_argument("-v", "--num-voters", dest="voters", default=10)
+    parser.add_argument("-c", "--num-candidates", dest="candidates", default=3)
+    parser.add_argument("-r", "--max-unique-rankings", dest="unique", default=10e10)
+    parser.add_argument("-maxrl", "--max-ranking-length", dest="max")
+    parser.add_argument("-minrl", "--min-ranking-length", dest="min")
+    parser.add_argument("-n", "--num-elections", dest="elections", default=1)
+    parser.add_argument("-o", "--output-file", dest="output")
+    parser.add_argument("-j", "--output-json", dest="json", default="true")
     args = parser.parse_args()
     return args
 
@@ -59,7 +57,7 @@ def generate_ballots(voters, rankings):
 def generate_elections(unique, candidates, min_len, max_len, num_elections, voters):
     '''Generate all elections'''
     elections = []
-    for i in range(num_elections):
+    for _ in range(num_elections):
 
         # Pull unique rankings and update status
         rankings = generate_unique_rankings(unique, candidates, min_len, max_len, voters)
@@ -69,16 +67,35 @@ def generate_elections(unique, candidates, min_len, max_len, num_elections, vote
         elections.append(ballots)
     return elections
 
-def output_elections(output, elections, voters, candidates, unique, min_len, max_len):
+def output_elections_json(output, elections, voters, candidates, unique, min_len, max_len):
     json_statement = {"num_voters": voters, "num_candidates": candidates, "max_unique_rankings": unique, \
                           "max_ranking_length": max_len, "min_ranking_length": min_len, "num_elections": len(elections), \
                             "elections": [{"ballots": [{"ranking": ranking[0], "count": ranking[1]} for ranking in election]} for election in elections]}
     if output is None:
         print(json.dumps(json_statement, indent=4))
     else:
-        file = open(output, "w")
-        file.write(json.dumps(json_statement, indent=4))
-        file.close()
+        with open(output, "w") as file:
+            file.write(json.dumps(json_statement, indent=4))
+
+def output_elections_csv(output, elections):
+    result = ""
+    for i, election in enumerate(elections):
+        for j, ranking in enumerate(election):
+            result += str(ranking[1]) + ","
+            for k, candidate in enumerate(ranking[0]):
+                if k != len(ranking[0]) - 1:
+                    result += str(candidate) + ","
+                else:
+                    result += str(candidate)
+            if j != len(election) - 1:
+                result += "\n"
+        if i != len(elections) - 1:
+            result += "\n\n"
+    if output is None:
+        print(result)
+    else:
+        with open(output, "w") as file:
+            file.write(result)
 
 def main():
     # Pull and process arguments
@@ -98,7 +115,11 @@ def main():
         min_len = int(min_len)
     num_elections = int(args.elections)
     output = args.output
+    json = args.json.lower()
 
     elections = generate_elections(unique, candidates, min_len, max_len, num_elections, voters)
-    output_elections(output, elections, voters, candidates, unique, min_len, max_len)
+    if json == 'true' or json == 't':
+        output_elections_json(output, elections, voters, candidates, unique, min_len, max_len)
+    else:
+        output_elections_csv(output, elections)
 main()
